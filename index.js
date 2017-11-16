9e70df0c7445468c37310edca29cb3279fc71658
josh0588 <joshualonghi@gmail.com>
2017-11-16 13:22:13 -0500

    Oauth2 in progress

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // Sample Request
-$.ajax({
-    url: 'https://www.bungie.net/Platform/Destiny2/Milestones/',
-    headers: {
-        'X-API-Key':'d3c3718995fb464ca66ddba314dc183a',
-        'Content-Type':'application/json'
-    },
-    method: 'GET',
-    dataType: 'json',
-    success: function(data){
-      console.log('succes: '+JSON.stringify(data));
-    }
-  });
+
+
+// $("#login").click(function(){
+//    alert("hi");
+//   $.ajax({
+//       url: 'https://www.bungie.net/en/oauth/authorize?client_id=22684&response_type=code&state=6i0mkLx79Hp91nzWVeHrzHG4',
+//       headers: {
+//           'X-API-Key':'d3c3718995fb464ca66ddba314dc183a',
+//           'Content-Type':'application/json'
+//       },
+//       method: 'GET',
+//       dataType: 'json',
+//       success: function(data){
+//         console.log('succes: '+JSON.stringify(data));
+//       }
+//     });
+
+// });
+// $.ajax({
+//       url: 'https://www.bungie.net/Platform/Destiny2/Milestones/',
+//       headers: {
+//           'X-API-Key':'d3c3718995fb464ca66ddba314dc183a',
+//           'Content-Type':'application/json'
+//       },
+//       method: 'GET',
+//       dataType: 'json',
+//       success: function(data){
+//         console.log('succes: '+JSON.stringify(data));
+//       }
+//     });
